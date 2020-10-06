650916884bc39cf5eb9e98a0975b2fbd5e0b8f2d
mdaijaj <aijaj18@navgurukul.org>
2020-10-06 07:47:54 +0530

    joint user and left user

--- public/client.js
+++ public/client.js
@@ -2,18 +2,41 @@ const socket = io()
 let name;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-var audio=new Audio('ting.mp3');
+var audio=new Audio('message.mp3');
+var join_user=new Audio('whatsapp.mp3')
 
+// message area append message func
+const append =(message, postion)=>{
+    const message_element= document.createElement("div");
+    message_element.innerText= message
+    message_element.classList.add('message')
+    message_element.classList.add(postion)
+    messageArea.append(message_element)
+}
+
+//valid input
 do {
     name = prompt('Please enter your name: ')
 } while(!name)
 
+//connect to new users
+socket.emit('new_user_jointed', name)
+
+//user jonted
+socket.on('user_jointed', (name)=>{
+    append(`${name} jointed group`, 'incoming')
+    join_user.play();
+})
+
+// message sent process when click enter
 textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
+        console.log("element", e)
         sendMessage(e.target.value)
     }
 })
 
+//sent message after click
 function sendMessage(message) {
     let msg = {
         user: name,
@@ -25,11 +48,11 @@ function sendMessage(message) {
     scrollToBottom()
     // audio.play();
 
-
     // Send to server 
     socket.emit('message', msg)
 }
 
+//message are append message
 function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
     let className = type
@@ -51,6 +74,13 @@ socket.on('message', (msg) => {
     scrollToBottom()
 })
 
+
+// left chat
+socket.on('left',(user)=>{
+    append(`${name} left the chat`, 'incoming')
+})
+
+//for scroll bar extend mesage
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
--- server.js
+++ server.js
@@ -1,30 +1,49 @@
 const express = require('express')
-
 const app = express()
 
 const http = require('http').createServer(app)
-
 app.use(express.static(__dirname + '/public'))
+
+
+
+// use of Socket tools
+const io = require('socket.io')(http)
 
 //endpoint
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+const users={};
 
-// use of Socket 
-const io = require('socket.io')(http)
-
-//connect to new users
+//connect to new user and other users
 var i=0
 io.on('connection', (socket) => {
-    console.log("connected..")
-    var sum=i+1
+    console.log("connected.." ) 
+    socket.on("new_user_jointed", (name)=>{
+        console.log("new user;-", name)
+        var aa=users[socket.id]=name;
+        console.log("aa:-", aa)
+        socket.broadcast.emit('user_jointed', name)
+        var sum=i+1
+    })
+    
+    
+    //message sent 
     socket.on('message', (name) => {
+        console.log("api is working here.....")
         console.log("new user", name)
+        users[socket.id]=name
         socket.broadcast.emit('message', name)
     })
 
+    //disconnect to users
+    socket.on('disconnect', (message, name) => {
+        console.log("users detail:-",users[socket.id])
+        // console.log("new_mesasge:-",  message)
+        socket.broadcast.emit('left', users[socket.id]=name);
+        delete users[socket.id]
+    })
 })
 
 //server connected
